b5d4e99ca4c201cf999f5e00d758ff944453e0f9
Cornelia Lahnsteiner <cornelia.lahnsteiner@prime-sign.com>
2021-11-18 22:46:04 +0100

    KEYCLOAK-847 Add support for step up authentication
    add step-up authentication test
    add acr mappings in tests
    
    KEYCLOAK-847 Fix behavior of unknown not essential acr claim
    
    
    add level of assurance tests
    
    fix acrValuesQueryParameter test
    
    Remove tests for unknown essential acr claim
    
    
    KEYCLOAK-847 Add headers and copyrights
    
    
    KEYCLOAK-847 remove wait.until from tests

--- quarkus/runtime/src/test/java/org/keycloak/provider/quarkus/ConfigurationTest.java
+++ quarkus/runtime/src/test/java/org/keycloak/provider/quarkus/ConfigurationTest.java
@@ -23,6 +23,7 @@ import static org.keycloak.quarkus.runtime.Environment.CLI_ARGS;
 
 import java.io.File;
 import java.lang.reflect.Field;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
