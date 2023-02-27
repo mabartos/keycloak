857eb7db74826aeea2c0405487bc5fbc0b934efd
Martin Bartoš <mabartos@redhat.com>
2023-02-27 15:01:08 +0100

    Use WildFly as the default app server

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/adapter/servlet/DemoFilterServletAdapterTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/adapter/servlet/DemoFilterServletAdapterTest.java
@@ -23,7 +23,7 @@ import org.keycloak.testsuite.utils.annotation.UseServletFilter;
 import org.keycloak.testsuite.utils.arquillian.ContainerConstants;
 
 /* App server must run with different version of Undertow - not possible right now */
-@AppServerContainer(value = ContainerConstants.APP_SERVER_UNDERTOW, skip = true)
+/*@AppServerContainer(value = ContainerConstants.APP_SERVER_UNDERTOW, skip = true)*/
 @AppServerContainer(ContainerConstants.APP_SERVER_WILDFLY)
 @AppServerContainer(ContainerConstants.APP_SERVER_EAP)
 @AppServerContainer(ContainerConstants.APP_SERVER_EAP6)
