38242b9341e2f29774bb8d5a6442cddd365e47ae
Martin Bartoš <mabartos@redhat.com>
2025-08-04 14:20:00 +0200

    Cannot use dev-file for additional datasources
    Closes #41641
    
    Signed-off-by: Martin Bartoš <mabartos@redhat.com>

--- quarkus/tests/integration/src/test/java/org/keycloak/it/cli/dist/CustomJpaEntityProviderDistTest.java
+++ quarkus/tests/integration/src/test/java/org/keycloak/it/cli/dist/CustomJpaEntityProviderDistTest.java
@@ -61,12 +61,17 @@ public class CustomJpaEntityProviderDistTest {
     }
 
     @Test
-    @Launch({"start-dev", "--db=dev-file", "--log-level=org.hibernate.jpa.internal.util.LogHelper:debug,org.keycloak.quarkus.deployment.KeycloakProcessor:debug", "--db-kind-new-user-store=dev-mem", "--db-kind-client-store=dev-mem", "--db-kind-pu-without-dialect-store=dev-mem"})
+    @Launch({"start-dev", "--db=dev-file", "--log-level=org.hibernate.jpa.internal.util.LogHelper:debug,org.keycloak.quarkus.deployment.KeycloakProcessor:debug", "--db-kind-new-user-store=dev-mem", "--db-kind-client-store=dev-file", "--db-kind-pu-without-dialect-store=dev-file"})
     void testUserManagedEntityNotAddedToDefaultPU(CLIResult cliResult) {
         cliResult.assertMessage(MULTIPLE_DATASOURCES_MSG);
         cliResult.assertMessage("Datasource name 'client-store' is obtained from the 'Persistence unit name' configuration property in persistence.xml file. Use 'client-store' name for datasource options like 'db-kind-client-store'.");
         cliResult.assertMessage("Datasource name 'pu-without-dialect-store' is obtained from the 'Persistence unit name' configuration property in persistence.xml file. Use 'pu-without-dialect-store' name for datasource options like 'db-kind-pu-without-dialect-store'.");
         cliResult.assertMessage("Datasource name 'new-user-store' is obtained from the 'jakarta.persistence.jtaDataSource' configuration property in persistence.xml file. Use 'new-user-store' name for datasource options like 'db-kind-new-user-store'.");
+
+        // tests for https://github.com/keycloak/keycloak/issues/41641
+        cliResult.assertNoMessage("(JPA Startup Thread: pu-without-dialect-store) Error while creating file");
+        cliResult.assertNoMessage("(JPA Startup Thread: client-store) Error while creating file");
+        cliResult.assertNoMessage("(JPA Startup Thread: keycloak-default) Error while creating file");
 
         cliResult.assertStringCount("name: new-user-store", 1);
         cliResult.assertStringCount("name: client-store", 1);
